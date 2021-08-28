8706ec36870157439b88eb218b2590de7c6f390f
Leszek Grechowicz <leszek_grechowicz@o2.pl>
2021-08-28 11:15:52 +0100

    Add module that create DB and TABLES

---
+++ create_db.py
@@ -0,0 +1,67 @@
+from psycopg2 import connect, OperationalError
+from psycopg2.errors import DuplicateDatabase, DuplicateTable
+
+USER = "postgres"
+HOST = "localhost"
+PASSWORD = "password"
+
+
+def create_db(db_name):
+    """
+    Create database named parm db_name using psycopg2.
+
+    :param str db_name: name of database,
+
+    :rtype:
+    :return: data from psycobg2 cursor as a list (can be None) if nothing to fetch.
+    """
+
+    sql_crate_db = f"CREATE DATABASE {db_name};"
+
+    sql_crate_tables = [f"""CREATE TABLE users (
+                        id serial,
+                        user_name varchar(255),
+                        hashed_password varchar(80),
+                        PRIMARY KEY(id)
+                    );""",
+
+                    f"""CREATE TABLE messages (
+                        id serial,
+                        cration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                        text varchar(255),
+                        from_id int REFERENCES users(id) ON DELETE CASCADE,
+                        to_id int REFERENCES users(id) ON DELETE CASCADE,
+                        PRIMARY KEY(id)
+                        );"""
+                    ]
+
+    try:
+        # connect to DB server and create database
+        cnx = connect(user=USER, password=PASSWORD, host=HOST)
+        cnx.autocommit = True
+        cursor = cnx.cursor()
+        cursor.execute(sql_crate_db)
+        print(f"Database {db_name} has been created.")
+
+        # connect to the crated DB and crate TABLES
+        cnx = connect(user=USER, password=PASSWORD, host=HOST, database=db_name)
+        cnx.autocommit = True
+        cursor = cnx.cursor()
+        for i, sql_query in enumerate(sql_crate_tables):
+            cursor.execute(sql_query)
+            print(f"Table {i} for {db_name} has been created.")
+
+    except OperationalError:
+        print("Error !")
+    except DuplicateDatabase as error:
+        print(error)
+    except DuplicateTable as error:
+        print(error)
+
+    else:
+        cursor.close()
+        cnx.close()
+
+
+if __name__ == '__main__':
+    create_db('test')
